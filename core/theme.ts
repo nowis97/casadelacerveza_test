050413eed3837972fb1a04ba0e252c611dfffaca
Simón Espinosa <simon.espinosa@vestua.com>
2022-06-19 23:50:53 -0400

    feat: add theme file

---
+++ core/theme.ts
@@ -0,0 +1,13 @@
+interface Theme {
+    primaryColor: string;
+    secondaryColor:string;
+    hoverColor: string;
+    borderRadius: number;
+}
+
+export const theme: Theme= {
+    borderRadius: 8,
+    primaryColor: '#f7b62c',
+    secondaryColor: '#000',
+    hoverColor: '#000'
+}
